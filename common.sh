159db5e8096b63edd3830c03ed16191f8a811d04
User Name <your_email@example.com>
2024-03-04 18:45:18 +0100

    added chrome installation and firefox removal

--- common.sh
+++ common.sh
@@ -30,6 +30,18 @@ sudo apt install -y build-essential
 # Vim
 sudo apt install -y vim
 
+# Chrome
+sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
+wget -O- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo tee /etc/apt/trusted.gpg.d/linux_signing_key.pub
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 78BD65473CB3BD13
+sudo apt-get update
+sudo apt-key export D38B4796 | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/chrome.gpg
+sudo apt-get install google-chrome-stable
+
+# Remove Firefox
+sudo apt remove firefox
+sudo rm -Rf /usr/bin/firefox
+sudo rm -Rf /usr/loca/firefox
+
 # Print completion message
 echo "System configuration completed."
-
