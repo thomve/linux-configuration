928dd7e06f9f1a06cbfd324756af85263a9bfb81
User Name <your_email@example.com>
2024-03-10 20:05:19 +0100

    added docker install from apt

--- ubuntu-common.sh
+++ ubuntu-common.sh
@@ -43,5 +43,22 @@ sudo apt remove firefox
 sudo rm -Rf /usr/bin/firefox
 sudo rm -Rf /usr/loca/firefox
 
+# Docker
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo docker run hello-world
+
+
 # Print completion message
 echo "System configuration completed."
